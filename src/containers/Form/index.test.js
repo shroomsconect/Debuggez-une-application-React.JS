7ff8b0cd0757aac7a95506c5a50db88da9a887bb
shroomsconect <stef.pachot@gmail.com>
2024-02-07 09:38:54 +0100

    Erreur dans deux tests, ils n'attendaient pas suffisament le changement de texte -Deux tests ont une erreur au moment de chercher un texte qui est censé mettre du temps à apparaître, ralongement du temps de recherche à 5000 millisecondes.

--- src/containers/Form/index.test.js
+++ src/containers/Form/index.test.js
@@ -22,7 +22,7 @@ describe("When Events is created", () => {
         })
       );
       await screen.findByText("En cours");
-      await screen.findByText("Envoyer");
+      await screen.findByText("Envoyer", undefined, {timeout: 5000});
       expect(onSuccess).toHaveBeenCalled();
     });
   });
